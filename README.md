da30ff171dcd7464ad63b98a510f3d71bb841e60
AniLeo-01 <aniruddha1.2.3.3.2.1@gmail.com>
2023-08-05 20:43:14 +0530

    Added requirements and updated prediction module

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Plastic-In-River-Detection
+
+## Objective
+
+Plastic pollution in marine environnement is a global threat. It threatens marine species health, human health, food security, costal tourism. More than 350 million tons of plastic are produced every year and it is estimated that more than 15 million tons end up in the world Ocean. The plastic then degrades over time into micro-plastic and both macro and microplastic have serious environmental impacts. Rivers are the very first source of plastic in Oceans. It is estimated that more than 80% of river plastic comes from only 1000 rivers.
+
+Plastic detection models could play a huge role in the struggle against plastic pollution. It could equip autonomous boats to clean rivers or analyze videos from many sources to detect areas with a high plastic density.
+
+Dataset source: [https://huggingface.co/datasets/Kili/plastic_in_river]()
+
+##
--- predict.py
+++ predict.py
@@ -1,18 +1,24 @@
 from ultralytics import YOLO
 from PIL import Image
+from datasets import load_dataset
 
-img = dataset['test'][20]['image']
+def predict(image: Image, model_path: str):
+    model = YOLO(model_path)
+    pred = model.predict(image)[0]
+    # print(pred.boxes)
+    #plotting the image with bounding boxes
+    pred = pred.plot(line_width=1)
+    #convert from BGR to RGB
+    pred_rgb = pred[..., ::-1]
+    pred_img = Image.fromarray(pred_rgb)
+    return pred_img
 
-#choosing the best training checkpoint
-model = YOLO('runs/detect/train/weights/best.pt')
+if __name__ == "__main__":
+    dataset = load_dataset('Kili/plastic_in_river', num_proc=12)
+    id = 20
+    img = dataset['test'][id]['image']
+    #choosing the best training checkpoint
+    model_path = 'runs/detect/train/weights/best.pt'
+    pred_img = predict(image=img, model_path=model_path)
+    pred_img.save('output.png')
 
-pred = model.predict(img)[0]
-print(pred.boxes)
-
-#plotting the image with bounding boxes
-pred = pred.plot(line_width=1)
-#convert from BGR to RGB
-pred_rgb = pred[..., ::-1]
-pred_img = Image.fromarray(pred_rgb)
-
-pred_img.save('output.png')
